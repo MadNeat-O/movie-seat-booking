22c5b647f188b66e6f2d829de6f090ffd976381e
Brad Korman <brad.korman@remarque-creative.com>
2020-08-06 13:15:39 -0400

    js functions added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('test');
+$(document).ready(function(){
+  // Get Elements
+  const container = $('.container')
+  const seats = $('.row .seat:not(.occupied)')
+  const count = $('#count')
+  const total = $('#total')
+  const movie = $('#movie')
+  let ticketPrice = +movie[0].value
+
+  // Functions
+  const updateSelectedCount = () => {
+    const selectedSeats = $('.row .seat.selected')
+    const selectedSeatsCount = selectedSeats.length
+    count.text(selectedSeatsCount)
+    total.text(selectedSeatsCount * ticketPrice)
+  }
+
+
+  // Event listeners
+  movie.change(e => {
+    ticketPrice = +e.target.value
+    updateSelectedCount()
+  })
+
+  container.click(e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+      e.target.classList.toggle('selected')
+
+      updateSelectedCount()
+    }
+  })
+
+});
